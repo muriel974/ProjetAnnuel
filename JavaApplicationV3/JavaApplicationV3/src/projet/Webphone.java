f9cf10ff0cf8b220ecb42f873826b0c835df55f3
Clement Casamayou <clement.casamayou@gmail.com>
2013-03-18 15:44:36 +0100

    test

--- JavaApplicationV3/JavaApplicationV3/src/projet/Webphone.java
+++ JavaApplicationV3/JavaApplicationV3/src/projet/Webphone.java
@@ -1,8 +1,7 @@
 /*
  * To change this template, choose Tools | Templates
- * and open the template in the editor. cacaantonin
+ * and open the template in the editor.
  */
-/** clement n'est pas relié**/
 /**
  *
  * @author Clement
